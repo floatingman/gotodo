710d29b5226befa03e5b8883dfa32ad087fda497
Daniel Newman <dan@danlovesprogramming.com>
2023-11-16 16:55:55 -0600

    Changed flag output

--- todo/cmd/todo/main.go
+++ todo/cmd/todo/main.go
@@ -11,6 +11,13 @@ import (
 const todoFileName = ".todo.json"
 
 func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "%s tool\n Developed by Monkies\n", os.Args[0])
+		fmt.Fprintf(flag.CommandLine.Output(), "Copyright 2023\n")
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage information:\n")
+		flag.PrintDefaults()
+	}
+
 	// Parsing command line flags
 	task := flag.String("task", "", "Task to be included in the ToDo list")
 	list := flag.Bool("list", false, "List all tasks")
